e23d14e7d9c8420966fd0c471a7fe003317cd184
malegaonkar-k <malegaonkar.k@yahoo.com>
2018-02-11 16:29:19 +0530

    Added Bootstrap support

--- src/App/components/App.js
+++ src/App/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component,} from 'react';
 
+import {Bootstrap, Grid, Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 export class App extends Component {
 
     render() {
             console.log("store :",this.props)
         return (
             <div>
+                <div className="display-1"> its bootstrap!!!</div>
+                <div > its bootstrap!!!</div>
                 <h1>{this.props.geod.title || 'Hello World!'}</h1>
                 {this.props.geod.title ?
                     <button onClick={this.props.closeGeod}>
@@ -27,3 +33,4 @@ export class App extends Component {
 
 }
 
+//https://www.reactpwa.com/docs/pages-routing/page-with-layout
